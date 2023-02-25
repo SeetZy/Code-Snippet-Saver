e68517fc7de0ff9a36543db2a8b13a8af74fc89f
Patriks Raiss-Reiss <64326465+SeetZy@users.noreply.github.com>
2023-02-25 16:55:34 +0200

    hopefully should be working

--- backend/main.js
+++ backend/main.js
@@ -22,7 +22,7 @@ connectDb()
 const app = express()
 
 // Define the port
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
